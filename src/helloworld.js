d29cced7da46795dc47b3f0ffb0aeaf0f9e94dce
callmekungfu <callmekungfu@users.noreply.github.com>
2018-11-18 11:44:09 -0500

    Typescript + Webpack - Wrote some code in Typescript - Installed Webpack

---
+++ src/helloworld.js
@@ -0,0 +1,9 @@
+class Startup {
+    static main() {
+        console.log('Hello World');
+        console.log('autochange wow');
+        return 0;
+    }
+}
+Startup.main();
+//# sourceMappingURL=helloworld.js.map
---
+++ src/helloworld.ts
@@ -0,0 +1,9 @@
+class Startup {
+    public static main(): number {
+        console.log('Hello World');
+        console.log('autochange wow');
+        return 0;
+    }
+}
+
+Startup.main();
