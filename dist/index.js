82a136df44a183427853f6731b2d6ef1c387e1aa
callmekungfu <callmekungfu@users.noreply.github.com>
2018-11-19 20:46:50 -0500

    Typescript React

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDOM = require("react-dom");
+var hello_1 = require("./components/hello");
+ReactDOM.render(React.createElement(hello_1.Hello, { compiler: "TypeScript", framework: "React" }), document.getElementById('example'));
+//# sourceMappingURL=index.js.map
