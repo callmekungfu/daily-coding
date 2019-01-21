4bab0b3e7fca38927ed26d36c37e32ce06e50514
callmekungfu <callmekungfu@users.noreply.github.com>
2019-01-21 10:35:33 -0500

    Solution for question 1.3

---
+++ coding-interviews/Chapter 1/1.3.py
@@ -0,0 +1,11 @@
+# Cracking the Coding Interview 6th Edition
+# Chapter 1 - Arrays and Strings. Page 88
+# Question 1.2 URLify
+# Solved by Yong Lin Wang
+
+"""
+    Write a method to replace all spaces in a string with '%20: You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.) 
+"""
+
+def urlify(url):
+    return url.replace(' ','%20')
