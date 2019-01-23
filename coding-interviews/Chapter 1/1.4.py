687938d54c2370a85b5a8190a9345da35867bc7a
callmekungfu <callmekungfu@users.noreply.github.com>
2019-01-23 15:41:04 -0500

    Incomplete version of the 1.4

--- coding-interviews/Chapter 1/1.4.py
+++ coding-interviews/Chapter 1/1.4.py
@@ -7,6 +7,8 @@
     Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. 
 """
 
+from random import shuffle
+
 def palindrome_detector(text):
     string_map = {}
     text = text.replace(' ', '')
@@ -28,7 +30,30 @@ def palindrom_builder(text):
     if not palindrome_detector(text):
         raise ValueError("String Provided is Not Palindromable.")
     
-    characters = text.split('')
+    characters = list(text)
+    characters = list(filter((' ').__ne__, characters))
+
+    single_char = ''
+
+    for char in characters: 
+        if characters.count(char) == 1:
+            characters.remove(char)
+            single_char = char
+    
+    shuffle(characters)
+    length = len(characters) // 2
+    i = 0
+
+    buffer = ''
+
+    while len(characters) > 0:
+        if length // 2 == i:
+            buffer += single_char
+        else:
+            buffer += characters[0]
+            characters = list(filter((characters[0]).__ne__, characters))
+        i += 1
+    print(buffer)
     
 
-print(palindrome_detector(input("Your Text: ").strip()))
+palindrom_builder(input("Your Text: ").strip())
